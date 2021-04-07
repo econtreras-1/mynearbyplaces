42ed291f3f9ac5a321697112e1dbc1a507eccad3
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-04-07 11:13:27 -0700

    Commit Partial Components

---
+++ mynearbyplaces/src/Components/data.js
@@ -0,0 +1,42 @@
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+
+let business_type = [
+    {
+        id: 0,
+        name: "Prep & Pastry",
+        business_type: "Restaurant",
+        image: "https://s3-media0.fl.yelpcdn.com/bphoto/gdXA2hAQwsUkCnIu8aFfow/o.jpg",
+        address: "3310 E 22nd St, Tucson, AZ 85713"
+
+    },
+
+    {
+        id: 1,
+        name: "Baja Cafe",
+        business_type: "Restaurant",
+        image: "https://s3-media0.fl.yelpcdn.com/bphoto/vj34Pa6Nxgcvcc3wyUeq7A/o.jpg",
+        address: "7002 E Broadway Blvd, Tucson, AZ 85710"
+
+    },
+
+    {
+        id: 2,
+        name: "Bobo's Restaurant",
+        business_type: "Restaurant",
+        image: "https://s3-media0.fl.yelpcdn.com/bphoto/XrBx-KC2wDv6JY2JnP8bhw/o.jpg",
+        address: "2938 E Grant Rd, Tucson, AZ 85716"
+
+    },
+    {
+        id: 3,
+        name: "Seis Kitchen",
+        business_type: "Restaurant",
+        image: "https://s3-media0.fl.yelpcdn.com/bphoto/vj34Pa6Nxgcvcc3wyUeq7A/o.jpg",
+        address: "130 S Avenida Del Convento, Tucson, AZ 85745"
+
+    },
+];
+export default business_type;
