c85e74c56b5c07e6aaadb6cb877ccca057d4dad5
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-04-07 10:51:59 -0700

    Commit All

--- mynearbyplaces/src/App.js
+++ mynearbyplaces/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import {HashRouter, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Add from './components/Add';
+import AddReview from './components/AddReview';
+import Delete from './components/Delete';
+import Review from './components/Review';
+import Edit from './components/Edit';
+import NavigationBar from './components/NavigationBar';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import business from './components/data'
+import React, { useEffect, useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [username, setUsername] = useState(localStorage.getItem('username') || '');
+  const [list, setList] = useState(business)
+  let onLoggedIn = (email) => {
+    localStorage.setItem('username', email);
+    setUsername(email);
+  }
 
+
+  return (
+    <HashRouter>
+      <Container fluid>
+        <Header />
+        <NavigationBar username={username} />
+        <Switch>
+          <Route exact path="/" component={Home}>
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login onLoggedIn={onLoggedIn} />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+          <Route path="/delete" component={Delete}>
+            <Delete />
+          </Route>
+          <Route path="/edit" component={Edit}>
+            <Edit />
+          </Route>
+          <Route path="/review" component={Review}>
+            <Review />
+          </Route>
+          <Route path="/addreview" component={AddReview}>
+            <AddReview />
+          </Route>
+        </Switch>
+        <Footer />
+      </Container>
+    </HashRouter>
+  );
+};
 export default App;
